e482cc5346aa65532a2d738110f6071af4bf4d85
asoyuta <asoyuta@icloud.com>
2021-10-06 16:11:01 +0900

    commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4-FzGgWByitoqJsm_goUYP3-P3saE2b4",
+  authDomain: "ec-app-15774.firebaseapp.com",
+  projectId: "ec-app-15774",
+  storageBucket: "ec-app-15774.appspot.com",
+  messagingSenderId: "850740802653",
+  appId: "1:850740802653:web:8663f997a72385cfc249b4",
+  measurementId: "G-HBG7VX5P04"
+};
